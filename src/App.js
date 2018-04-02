38735040e1d852c214f57b983300426cabe3bd41
Michael Hoe <michaelpaulhoe@gmail.com>
2018-04-02 14:27:22 +0100

    Nicer looking routing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Header from './views/header';
+
+//Page Imports
 import Home from './views/HomePage';
 import Calendar from './views/CalendarPage';
 
@@ -10,20 +11,29 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
         <Router>
         <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <h1 className="navbar-brand mb-0 h1">Willand Village Hall</h1>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <span className="nav-link">
+                <Link to="/">Home</Link>
+              </span>
             </li>
-            <li>
+            <li className="nav-item">
+            <span className="nav-link">
               <Link to="/calendar">Events Calendar</Link>
+              </span>
             </li>
           </ul>
 
-          <hr />
-
+          </div>
+        </nav>
           <Route exact path="/" component={Home} />
           <Route exact path="/calendar" component={Calendar} />
         </div>
--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -9,7 +9,7 @@ class Body extends Component {
             <div className="card">
                 <div className="card-body">
                     <h2 className="card-title">About Us</h2>
-                    <p class="card-text">
+                    <p className="card-text">
                         Willand Village Hall, one of the largest in Devon, is situated just off the B3181, in Gables Road, Willand with easy access from all local areas and from junctions 27 and 28 of the M5. 
                         The Hall has a Public Entertainment and Theatre Licence under the Licensing Act 2003. The hall also benefits from having central heating throughout.
                     </p>
