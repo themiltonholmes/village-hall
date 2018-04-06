8ce291ca6453c4217a795a2e3b8297456a8d723c
Michael Hoe <michaelpaulhoe@gmail.com>
2018-04-06 17:48:32 +0100

    Google Maps added to contacts page

--- src/views/ContactPage.js
+++ src/views/ContactPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Page from './PageComponent';
 import ContactCard from './cards/contactCard';
+import MapsContainer from './mapsContainer';
 
 
 const contacts = [
@@ -32,11 +33,20 @@ class ContactPage extends Component {
     render() {
         return (
             <Page title="contact-us">
+            <div className="row">
+            <div className="col-md-6">
+                <h2>Contacts</h2>
                 {contacts.map( (contact) => {
                     return <div key={contact.name}>
                     <ContactCard {...contact} />
                     </div>
                 })}
+                </div>
+                <div className="col-md-6">
+                <h2>Find Us</h2>
+                <MapsContainer />
+                </div>
+                </div>
             </Page>
         );
     }
