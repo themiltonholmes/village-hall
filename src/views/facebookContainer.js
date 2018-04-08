444b230f973a520ece2d11005d3ebed7d4969105
Michael Hoe <michaelpaulhoe@gmail.com>
2018-04-08 15:06:48 +0100

    Fixed the facebook plugin. Take that! removed react-facebook

--- src/views/facebookContainer.js
+++ src/views/facebookContainer.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import FacebookProvider, { Page } from 'react-facebook';
 
 const facebookUrl = "https://www.facebook.com/WillandVillageHall/";
 
@@ -26,15 +25,14 @@ class FacebookContainer extends Component {
   updateDimensions() {
     const containerWidth = this.facebookContainerElement.clientWidth;
     this.setState({width: containerWidth});
+    this.url = `https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FWillandVillageHall&tabs=timeline&width=${containerWidth}&height=500&small_header=false&adapt_container_width=true&hide_cover=true&show_facepile=true&appId`
   }
 
   render() {
     return (
       <div id="facebook-container">
-            <div ref={this.facebookContainer} className="big-facebook col-12">
-            <FacebookProvider appId="willandVillageHall">
-              <Page hideCover={true} width={this.state.width} href={facebookUrl} tabs="timeline" />
-            </FacebookProvider>
+            <div ref={this.facebookContainer} className="big-facebook">
+            <iframe className="facebook-frame" width={this.state.width} src={this.url}></iframe>
             </div>
             <div className="small-facebook">
               <a target="_blank" href={facebookUrl}><span className="btn btn-primary col-12">Visit Our Facebook Page</span></a>
