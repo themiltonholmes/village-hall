be985c6d963751ba0a14ae3ccbb3aed73d8a6e84
Michael Hoe <michaelpaulhoe@gmail.com>
2018-04-08 14:49:15 +0100

    More name changes

--- src/views/cards/bookingCard.js
+++ src/views/cards/bookingCard.js
@@ -5,11 +5,11 @@ const BookingCard = () => {
         <div className="card">
             <div className="card-body">
                 <h2 className="card-title">Booking the Hall</h2>
-                <p>Please read <a target="_blank" href={`${process.env.PUBLIC_URL}/Documents/Wvh_Hire_Conditions_2018.pdf`}>Our Hire Conditions</a></p>
+                <p>Please read <a target="_blank" href={`${process.env.PUBLIC_URL}/Documents/Wvh_Hire_Conditions.pdf`}>Our Hire Conditions</a></p>
                 <div className="form-link-container">
                     <ul className="list-group list-group-flush">
-                    <li className="list-group-item"><a href={`${process.env.PUBLIC_URL}/Documents/Wvh_Booking_Form_single_Event_2018_-_2019.pdf`} target="_blank" id="booking-btn">Single Event</a></li>
-                    <li className="list-group-item"><a href={`${process.env.PUBLIC_URL}/Documents/Wvh_Regular_Booking_Form_2018_-_2019.pdf`} target="_blank" id="booking-btn">Reoccuring Events</a></li>
+                    <li className="list-group-item"><a href={`${process.env.PUBLIC_URL}/Documents/Wvh_Booking_Form_single_Event.pdf`} target="_blank" id="booking-btn">Single Event</a></li>
+                    <li className="list-group-item"><a href={`${process.env.PUBLIC_URL}/Documents/Wvh_Regular_Booking_Form.pdf`} target="_blank" id="booking-btn">Reoccuring Events</a></li>
                 </ul>
                 </div>
             </div>
