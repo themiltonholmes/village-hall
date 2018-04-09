e14df153c4fcaaef202f2df8ee1ef51cc4311d56
Michael Hoe <michaelpaulhoe@gmail.com>
2018-04-09 16:58:31 +0100

    Goodbye manifest

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+unregister();
--- src/views/cards/bookingCard.js
+++ src/views/cards/bookingCard.js
@@ -8,9 +8,9 @@ const BookingCard = () => {
                 <p>Please read <a target="_blank" href={`${process.env.PUBLIC_URL}/Documents/Wvh_Hire_Conditions.pdf`}>Our Hire Conditions</a></p>
                 <div className="form-link-container">
                     <ul className="list-group list-group-flush">
-                    <li className="list-group-item"><a href={`${process.env.PUBLIC_URL}/Documents/Wvh_Booking_Form_single_Event.pdf`} target="_blank" id="booking-btn">Single Event</a></li>
-                    <li className="list-group-item"><a href={`${process.env.PUBLIC_URL}/Documents/Wvh_Regular_Booking_Form.pdf`} target="_blank" id="booking-btn">Reoccuring Events</a></li>
-                </ul>
+                        <li className="list-group-item"><a href={`${process.env.PUBLIC_URL}/Documents/Wvh_Booking_Form_single_Event.pdf`} target="_blank" id="booking-btn">Single Event Bookings</a></li>
+                        <li className="list-group-item"><a href={`${process.env.PUBLIC_URL}/Documents/Wvh_Regular_Booking_Form.pdf`} target="_blank" id="booking-btn">Reoccuring Events Bookngs</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
