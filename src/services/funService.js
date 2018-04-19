afbcad76f3a5dc281583408668ec68e9fe2e1c00
themiltonholmes <themiltonholmes@gmail.com>
2018-04-19 17:38:15 +0100

    Fixed footer positioning

--- src/services/funService.js
+++ src/services/funService.js
@@ -3,7 +3,7 @@
  * @param {*} success 
  */
 export const fetchWordOfTheDay = (success) => {
-    fetch(`http://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5`)
+    fetch(`https://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5`)
 	.then(function(response) {
 		if (response.status >= 400) {
             console.error("Word of the day doesn't work anymore. Call the developer and tell him to take it out");
